6b282c74c0573e2ca5343f4278dd9214233214df
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-21 14:13:29 +0600

    feat: add Subject model and update DatabaseSeeder with initial academic data

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    protected $fillable = ['department', 'semester', 'subject_code', 'subject_name', 'credit'];
+    protected $fillable = [
+        'department',
+        'semester',
+        'technology_code',
+        'technology_name',
+        'subject_code',
+        'subject_name',
+        'credit',
+        'theory_marks',
+        'practical_marks',
+        'total_marks',
+    ];
 }
